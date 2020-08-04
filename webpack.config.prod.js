ac05f2379a8bb99ad74cee18651c9575383733a7
David Lavieri <daviddlavier@gmail.com>
2020-08-04 15:52:20 +0200

    update deps, replace uglyfy with terser

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const plugins = [
@@ -8,8 +8,12 @@ const plugins = [
 
 const optimization = {
   minimizer: [
-    new UglifyJsPlugin({
-      sourceMap: true
+    new TerserPlugin({
+      sourceMap: true,
+      parallel: true,
+      terserOptions: {
+        ecma: 6
+      }
     })
   ],
   splitChunks: {
